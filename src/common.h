7fcd999b94b9752dc9fe96975f5da5bd16bc7b5a
Wenxiang Yang <jukisan0525@gmail.com>
2022-12-20 19:08:16 +0900

    simple connect

---
+++ src/common.h
@@ -0,0 +1,8 @@
+//
+// Created by yang on 22/12/20.
+//
+
+#ifndef ASIO_TEST_COMMON_H
+#define ASIO_TEST_COMMON_H
+#define ASIO_STANDALONE
+#endif //ASIO_TEST_COMMON_H
--- src/main_client.cpp
+++ src/main_client.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include "common.h"
 #include <asio.hpp>
 #include <vector>
+
 using asio::ip::tcp;
 int main(int argc, char* argv[])
 {
-    try {
-        if (argc != 2) {
-            std::cerr << "Usage: client <host>" << std::endl;
+    try
+    {
+        if (argc != 3)
+        {
+            std::cerr << "Usage: client <host> <port>" << std::endl;
             return 1;
         }
 
         std::cout << "Here is client!" << std::endl;
         asio::io_context context;
         tcp::resolver resolver(context);
-        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
+        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], argv[2]);
         tcp::socket socket(context);
         asio::connect(socket, endpoints);
         while (true)
         {
+            //std::cout << '?' << std::endl;
             std::vector<char> buffer(128);
             asio::error_code errorCode;
             size_t length = socket.read_some(asio::buffer(buffer), errorCode);
@@ -26,11 +31,14 @@ int main(int argc, char* argv[])
             else if (errorCode)
                 throw asio::system_error(errorCode);
             std::cout.write(buffer.data(), buffer.size());
+            std::string return_string("Success read byte: ");
+            return_string += std::to_string(buffer.size());
+            asio::write(socket, asio::buffer(return_string));
         }
     }
-    catch (std::exception e)
+    catch (std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cout << e.what() << std::endl;
     }
     return 0;
 }
